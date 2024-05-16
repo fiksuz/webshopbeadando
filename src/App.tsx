fa8e141fbb4f5d47b2fd1a493dcd9b60f64859d6
fiksuz <fiksuz@gmail.com>
2024-05-16 13:03:37 +0200

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import NavBar from "./components/NavBar";
+import Registration from './components/Registration';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import "bootstrap/dist/css/bootstrap.css";
+
 
 function App() {
+  const navItems = ["Home", "Regisztráció", "Bejelentkezés", "Termékek"];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+      <Router>
+        <NavBar brandName={"Webshop"} items={navItems} />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path='/regisztracio' element={<Registration/>} />
+        </Routes>
+      </Router>
+    
   );
 }
 
